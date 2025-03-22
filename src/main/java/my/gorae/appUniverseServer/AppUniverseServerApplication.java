9b366f4643fc59602c748fbb030fdb8e8249e4d8
MatchaKim <kimjunsik333@gmail.com>
2025-03-23 00:02:07 +0900

    feat #1 : BaseEntity 추가

--- src/main/java/my/gorae/appUniverseServer/AppUniverseServerApplication.java
+++ src/main/java/my/gorae/appUniverseServer/AppUniverseServerApplication.java
@@ -2,8 +2,10 @@ package my.gorae.appUniverseServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppUniverseServerApplication {
 
 	public static void main(String[] args) {
