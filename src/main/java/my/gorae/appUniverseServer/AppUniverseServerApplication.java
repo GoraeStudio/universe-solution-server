bdcb579de12073f4cea83cc6bec0b2f4223b13ad
MatchaKim <kimjunsik333@gmail.com>
2025-03-22 23:11:31 +0900

    init

---
+++ src/main/java/my/gorae/appUniverseServer/AppUniverseServerApplication.java
@@ -0,0 +1,13 @@
+package my.gorae.appUniverseServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUniverseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUniverseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/gorae/appUniverseServer/AppUniverseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.gorae.appUniverseServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUniverseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
