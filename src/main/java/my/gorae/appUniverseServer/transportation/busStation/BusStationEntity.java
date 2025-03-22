10934b429e0492fc4631354368d88f655cb60d60
MatchaKim <kimjunsik333@gmail.com>
2025-03-23 01:15:01 +0900

    feat #1 : BusStationEntity 구성

--- src/main/java/my/gorae/appUniverseServer/transportation/busStation/BusStationEntity.java
+++ src/main/java/my/gorae/appUniverseServer/transportation/busStation/BusStationEntity.java
@@ -1,4 +1,17 @@
 package my.gorae.appUniverseServer.transportation.busStation;
 
-public class BusStationEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import my.gorae.appUniverseServer.common.BaseEntity;
+
+@Entity
+public class BusStationEntity extends BaseEntity {
+
+    @Id
+    private Long id;
+
+    private String busStationName; // 정류소명
+    private float latitude; // 위도
+    private float longitude; // 경도
+
 }
